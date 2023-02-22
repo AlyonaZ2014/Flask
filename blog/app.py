d11f40166aff68f44c349053fa8a838591737e03
AlyonaZ <gorina.a.a@mail.ru>
2023-02-22 21:02:38 +0200

    index view dz2

--- blog/app.py
+++ blog/app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+# from flask import Flask
+from flask import Flask, render_template
 from flask import request
 from flask import g
 from werkzeug.exceptions import BadRequest
+from blog.views.users import users_app
+from blog.views.articles import articles_app
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+app.register_blueprint(users_app, url_prefix="/users")
+app.register_blueprint(articles_app, url_prefix="/articles")
+
 
 app = Flask(__name__)
 @app.route("/")

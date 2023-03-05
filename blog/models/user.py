5b6302b955f5bc7cef5a176e818c394932ca2b3d
AlyonaZ <gorina.a.a@mail.ru>
2023-03-05 16:13:28 +0200

    dz6

--- blog/models/user.py
+++ blog/models/user.py
@@ -2,12 +2,14 @@ from sqlalchemy import Column, Integer, String, Boolean, LargeBinary
 from blog.models.database import db
 from flask_login import UserMixin
 from blog.security import flask_bcrypt
+from sqlalchemy.orm import relationship
 
 class User(db.Model, UserMixin):
     id = Column(Integer, primary_key=True)
     username = Column(String(80), unique=True, nullable=False)
     is_staff = Column(Boolean, nullable=False, default=False)
     _password = Column(LargeBinary, nullable=True)
+    author = relationship("Author", uselist=False, back_populates="user")
 
     def __repr__(self):
         return f"<User #{self.id} {self.username!r}>"
